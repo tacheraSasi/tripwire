99bbbd442befe9335187bb1117eae09037da0801
tacherasasi <tacherasasi@gmail.com>
2025-05-11 13:29:30 +0300

    3rt

--- main.go
+++ main.go
@@ -1,15 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"os"
 
 	errorshandler "github.com/tacheraSasi/tripwire.git/errorsHandler"
+	"github.com/tacheraSasi/tripwire.git/utils"
 )
-
 
 func main() {
 	filename := "nonexistent.txt"
 	file, err := os.Open(filename)
-	errorshandler.Panic(err,"an error occurred while opening the file"+filename)
+	output := utils.Ternary(err != nil, err, nil)
+	fmt.Println("Output", output)
+	errorshandler.Panic(err, "an error occurred while opening the file"+filename)
 	defer file.Close()
 }
--- utils/utils.go
+++ utils/utils.go
@@ -3,3 +3,12 @@ package utils
 import "fmt"
 
 var Print = fmt.Println
+
+// Ternary operations in golang
+func Ternary[T any](condition bool, trueVal, falseVal T) T {
+	if condition {
+		return trueVal
+	}
+
+	return falseVal
+}
